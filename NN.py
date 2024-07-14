b2ff53453343a91e6e94367b2faa1d9c62023698
Rennan555 <dungeonbr5@gmail.com>
2024-07-14 18:45:03 -0300

    Predição e ciclo como feature

--- NN.py
+++ NN.py
@@ -1,8 +1,9 @@
+import numpy as np
 from extract_csv import load_carbon_csv_content
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
-classes = ['normal', 'alerta', 'anômalo']
+classes = ['normal', 'alert', 'anomaly']
 num_classes = len(classes)
 
 data, target = load_carbon_csv_content('dataset/dataset_carbon_filter.csv')
@@ -20,3 +21,18 @@ model.add(tf.keras.layers.Dense(num_classes))
 model.compile(optimizer='adam', 
               loss='mean_squared_error', 
               metrics=['accuracy' ,'mean_absolute_error'])
+
+X_train = np.asarray(X_train).astype(np.float32)
+y_train = np.asarray(y_train).astype(np.float32)
+model.fit(X_train, y_train, epochs=20, batch_size=32)
+
+X_test = np.asarray(X_test).astype(np.float32)
+y_test = np.asarray(y_test).astype(np.float32)
+loss, accuracy, mae = model.evaluate(X_test, y_test)
+print(f"Loss: {loss}, Accuracy: {accuracy}, MAE: {mae}")
+
+predictions = model.predict(X_test)
+predicted_classes = np.argmax(predictions, axis=1)
+print(predictions)
+print(predicted_classes)
+print(y_test)
--- extract_csv.py
+++ extract_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def load_carbon_csv_content(dir: str):
     csv_obj = pd.read_csv(dir)
-    data = np.array((csv_obj['campaign_E'], csv_obj['resistance_E']))
+    data = np.array((csv_obj['campaign_E'], csv_obj['resistance_E'], csv_obj['cycle_E']))
     data = data.T
     target = np.array(csv_obj['target_cycle'])
     target = class_enumerate(target)
@@ -11,7 +11,7 @@ def load_carbon_csv_content(dir: str):
 
 def class_enumerate(target):
     for i in range(len(target)):
-        if target[i] == 'normal': target[i] = 1
-        elif target[i] == 'alerta': target[i] = 2
-        elif target[i] == 'anômalo': target[i] = 3
+        if target[i] == 'normal': target[i] = 0
+        elif target[i] == 'alert': target[i] = 1
+        elif target[i] == 'anomaly': target[i] = 2
     return target
